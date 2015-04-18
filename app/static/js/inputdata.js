cc18c6b86bde0b1c1ee6369ef133f4c689f7fd12
Aishwarya Rajagopal <aishwarya.rajagopal@listertechnologies.com>
2015-04-18 14:59:47 -0400

    Pushing changes

---
+++ app/static/js/inputdata.js
@@ -0,0 +1,40 @@
+window.input_data = {
+    "name": "size",
+    "children": [
+        {
+            "name": "comfortable",
+            "children": [
+                {
+                    "name": "color",
+                    "children": [
+                        {
+                            "name": "slipping"
+                        },
+                        {
+                            "name": "gifts"
+                        }
+                    ]
+                },
+                {
+                    "name": "quality"
+                }
+            ]
+        },
+        {
+            "name": "fit",
+            "children": [
+                {
+                    "name": "elastic",
+                    "children": [
+                        {
+                            "name": "sprinter"
+                        },
+                        {
+                            "name": "spikes"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
